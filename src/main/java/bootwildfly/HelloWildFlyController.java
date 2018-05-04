b2c82cc394537c8ad9f296588b45aab397dec625
Mohamed Hedi ABIDI <abidimh@gmail.com>
2018-05-04 16:26:38 +0200

    whoami

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -2,12 +2,7 @@ package bootwildfly;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
-
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.net.InetAddress;
 
 @RestController
 public class HelloWildFlyController {
@@ -23,39 +18,8 @@ public class HelloWildFlyController {
         return ("My Version : 4.0.0");
     }
     
-    @RequestMapping(value="/info", method= RequestMethod.GET)
-    protected String doGet() throws Exception {
-
-        StringBuilder builder = new StringBuilder();
-        
-        long maxMemory = Runtime.getRuntime().maxMemory();
-
-        builder.append("<br/><br/>Available processors (cores): " + Runtime.getRuntime().availableProcessors())
-
-        .append("<br/><br/>Free memory (bytes): " + Runtime.getRuntime().freeMemory())
-        
-        .append("<br/>Maximum memory (bytes): " + (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory))
-
-        .append("<br/>Total memory available to JVM (bytes): " + Runtime.getRuntime().totalMemory());
-
-        File[] roots = File.listRoots();
-
-        for (File root : roots) {
-            builder.append("<br/><br/>File system root: " + root.getAbsolutePath())
-            .append("<br/>Total space (bytes): " + root.getTotalSpace())
-            .append("<br/>Free space (bytes): " + root.getFreeSpace())
-            .append("<br/>Usable space (bytes): " + root.getUsableSpace());
-        }
-        
-        builder.append("<br/><br/>Network Details:");
-        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
-        for (NetworkInterface netint : Collections.list(nets)) {
-            Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
-            for (InetAddress inetAddress : Collections.list(inetAddresses)) {
-                builder.append("<br/>Display name: "+ netint.getDisplayName()).append("\t\tInetAddress: "+ inetAddress);
-            }
-        }
-
+    @RequestMapping("whoami")
+    protected String whoami() throws Exception {
         // Handling UnknownHostException: name or service not known
         String hostName;
         try {
@@ -65,10 +29,6 @@ public class HelloWildFlyController {
             hostName = e.getMessage().split(":")[0];
         }
 
-        return
-            "<div style=\"text-align:center;\"><h1>Who Am I - "+hostName+"</h1></div>"
-            +"<div style=\"margin-left:10%\">"
-                +builder.toString()
-            +"</div>";
+        return "I'm " + hostName; 
     }
 }
